f39793803ec80875f7c874fb93a6322ba60d2aa9
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2020-09-11 21:25:55 +0200

    3: Add header in middleware in app.js to allow access by different clients. This is demonestrated in codepen in the lectures 8 & 9

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 const feedRoutes = require("./routes/feed");
 
@@ -11,7 +11,18 @@ const app = express();
 // app.use(bodyParser.urlencoded())  // x-www-form-urlencoded  <form>
 
 // When we get data as json. It uses application/json format
-app.use(bodyParser.json())      // application/json
+app.use(bodyParser.json()); // application/json
+
+// By default, all the clients are restricted to access the server/REST api endpoints
+// This is because of CORS, we solve this by allowing using headers in the response
+app.use((res, req, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // We could specify certain websites, however, * for all
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Autorization");
+});
 
 ///// Routes /////
 app.use("/feed", feedRoutes);
--- routes/feed.js
+++ routes/feed.js
@@ -7,6 +7,6 @@ const router = express.Router();
 // GET /feed/posts
 router.get("/posts", feedController.getPosts);
 // POST /feed/posts
-router.post("/posts", feedController.createPost);
+router.post("/post", feedController.createPost);
 
 module.exports = router;
