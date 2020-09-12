84b2073c14ce76e8ae72955fff5e8e401b1f60da
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2020-09-12 20:40:52 +0200

    4: Change getPosts & createPost in feed.js to match the frontend needs

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
-
 ///// Parse the body //////
-
+////////////////////////////
 // When we get data from <form>. It uses x-www-form-urlencoded format
 // app.use(bodyParser.urlencoded())  // x-www-form-urlencoded  <form>
 
@@ -15,16 +15,18 @@ app.use(bodyParser.json()); // application/json
 
 // By default, all the clients are restricted to access the server/REST api endpoints
 // This is because of CORS, we solve this by allowing using headers in the response
-app.use((res, req, next) => {
+app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // We could specify certain websites, however, * for all
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Autorization");
+  next();
 });
 
 ///// Routes /////
+//////////////////
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,7 +1,16 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
-      { title: "First post", content: "This is the content of the first post" },
+      {
+        _id: new Date().toDateString(),
+        title: "First post",
+        content: "This is the content of the first post",
+        creator: {
+          name: 'sahl',
+        },
+        imageUrl: 'images/Mongo.jpg',
+        createdAt: new Date(),
+      },
     ],
   });
 };
@@ -13,9 +22,13 @@ exports.createPost = (req, res, next) => {
   res.status(201).json({
     message: "Post created successfully",
     post: {
-      id: new Date().toISOString(),
-      title,
-      content,
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: {
+        name: "sahl",
+      },
+      createdAt: new Date(),
     },
   });
 };
