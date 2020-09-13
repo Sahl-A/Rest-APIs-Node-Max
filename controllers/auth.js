e367abdc244d518ebe95d58ee348ce0150122c0a
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2020-09-13 19:10:26 +0200

    13: Add signup validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,22 @@
-const User = require('../models/user')
+// Hash the password
+const bcrypt = require("bcrypt");
+// Get the validation result
+const { validationResult } = require("express-validator");
+
+const User = require("../models/user");
 
 // When clicking on signup button
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
+  // Check the validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error(`Validation failed: ${errors.array().msg}`);
+    error.statusCode = 422;
+    return next(error);
+  }
 
-}
+  // Get the data from body
+  const email = req.body.email;
+  const name = req.body.name;
+  const password = req.body.password;
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,35 @@
 const express = require("express");
 
+// Get the validation methods
+const { body } = require("express-validator");
+// Get the user to check email existance
+const User = require("../models/user");
 const authControllers = require("../controllers/auth");
 
 const router = express.Router();
 
 // POST /signup
-router.put("/signup", authControllers.signup);
+router.put(
+  "/signup",
+  [
+    body("email")
+      .trim()
+      .isEmail()
+      .withMessage("Invalid Email")
+      .custom(async (value) => {
+        // Check if the email exists in DB
+        const user = await User.findOne({ email: value });
+        if (user) {
+          return new Error("E-mail already in use");
+        }
+        return true;
+      }),
+    body("password", "Password should be more than 5 characters")
+      .trim()
+      .isLength({ min: 5 }),
+    body("name").trim().not().isEmpty(),
+  ],
+  authControllers.signup
+);
 
 module.exports = router;
